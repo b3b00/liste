76c4f92f671556a1fb97959c2745c19d5ed6f497
Olivier Duhart <1224790+b3b00@users.noreply.github.com>
2024-08-15 09:37:38 +0200

    Update Categories.svelt

--- public/Categories.svelte
+++ public/Categories.svelte
@@ -128,7 +128,7 @@
 
 <div style="display:flex;flex-direction: column;">
     {#each $categories as category}
-        <Button class="button-shaped-round" style="color:black;font-weight: bold;b-color:{category.color}" color="{category.color}" on:click={openEditor(true,category.label,category.color)}>{category.label} </Button>         
+        <Button class="button-shaped-round" style="color:black;font-weight: bold;background-color:{category.color}" color="{category.color}" on:click={openEditor(true,category.label,category.color)}>{category.label} </Button>         
     {/each}
 
 </div>
